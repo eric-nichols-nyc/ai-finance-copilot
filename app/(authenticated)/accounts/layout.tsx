c7f22440005ee9144d8876eb01aa45ffe594de0b
eric-nichols-nyc <ebn646@gmail.com>
2025-12-10 11:11:26 -0500

    feat: layout update

--- app/(authenticated)/accounts/layout.tsx
+++ app/(authenticated)/accounts/layout.tsx
@@ -17,16 +17,16 @@ export default function AccountsLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-6 lg:h-[calc(100vh-8rem)] lg:overflow-hidden">
       {/* Split Layout: 50/50 with border separator */}
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:h-full">
         {/* Left Panel - Persistent accounts list */}
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-6 lg:h-full lg:overflow-y-auto lg:pr-2">
           <AccountsLeftPanel />
         </div>
 
         {/* Right Panel - Dynamic content (children) with left border */}
-        <div className="flex flex-col gap-6 lg:border-l lg:pl-6">
+        <div className="flex flex-col gap-6 lg:border-l lg:pl-6 lg:h-full lg:overflow-y-auto">
           {children}
         </div>
       </div>
